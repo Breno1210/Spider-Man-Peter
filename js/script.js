bba68824e4b358022d28bca0e99376093f02d443
Breno <brenorosaalmeida@yahoo.com.br>
2024-03-19 19:06:32 -0300

    Added animation of the loading screen and elements - Project Completed

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
-
-/*==================== POINTS ====================*/ 
-const points = document.querySelectorAll('.point');
-const images = document.querySelectorAll('.img-slider');
+/*==================== POINTS ====================*/
+const points = document.querySelectorAll(".point");
+const images = document.querySelectorAll(".img-slider");
 
 let i = 0;
 
 points.forEach((item, index) => {
-  item.addEventListener('click', () => {
-    console.log("Clicou no ponto: " + (index));
+  item.addEventListener("click", () => {
+    console.log("Clicou no ponto: " + index);
 
     // Zera todas as imagens
     images[0].style.opacity = "0";
@@ -18,20 +17,18 @@ points.forEach((item, index) => {
     // Aparece somente aquela que clicou
     images[index].style.opacity = "1";
 
-
     points[0].classList.remove("active-point");
     points[1].classList.remove("active-point");
     points[2].classList.remove("active-point");
     points[3].classList.remove("active-point");
 
     points[index].classList.add("active-point");
-  })
-})
+  });
+});
 
 setInterval(changeBackground, 12000);
 
-function changeBackground(){
-
+function changeBackground() {
   // Zera todas as imagens
   images[0].style.opacity = "0";
   images[1].style.opacity = "0";
@@ -50,7 +47,7 @@ function changeBackground(){
 
   i++;
 
-  if(i == 4) i = 0;
+  if (i == 4) i = 0;
 
   //console.log('trocou de bg');
 }
@@ -59,46 +56,56 @@ function changeBackground(){
 //console.log(images[0]);
 
 /*--=========== GSAP SCROLLTRIGGER ============- */
-/* gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger);
 
 document.addEventListener("DOMContentLoaded", () => {
-
+  /*--=========== TIMELINE LOADING E HERO ============- */
 
   var tl1 = gsap.timeline();
-    
+
   tl1
-  .to('.screen-loading', {
-    duration: 2,
-    opacity: 0, 
-    delay: 3,
-    //filter: 'blur(10px)',
-    //y: "-10%",
-    ease: "power4.out",
-  })
-  .from(".bg-hero", {
-    duration: 1, 
-    opacity: 0, 
-    stagger:{each: 0.1},
-    ease: "power4.out",
-  }, '-=1.0')
-  .from("nav div", {
-    duration: 2, 
-    opacity: 0, 
-    y: 400,
-    stagger:{each: 0.2},
-    ease: "back.out(1.7)",
-  }, '-=0.5')
-  .from(".box-hero div", {
-    duration: 2, 
-    opacity: 0, 
-    y: 200,
-    stagger:{each: 0.2},
-    ease: "back.out(1.7)",
-  }, '-=1.7')
-    
+    .to(".screen-loading", {
+      duration: 2,
+      opacity: 0,
+      delay: 3,
+      //filter: 'blur(10px)',
+      //y: "-10%",
+      ease: "power4.out",
+    })
+    .from(
+      ".bg-hero",
+      {
+        duration: 1,
+        opacity: 0,
+        stagger: { each: 0.1 },
+        ease: "power4.out",
+      },
+      "-=1.0"
+    )
+    .from(
+      "nav div",
+      {
+        duration: 2,
+        opacity: 0,
+        y: 400,
+        stagger: { each: 0.2 },
+        ease: "back.out(1.7)",
+      },
+      "-=0.5"
+    )
+    .from(
+      ".box-hero div",
+      {
+        duration: 2,
+        opacity: 0,
+        y: 200,
+        stagger: { each: 0.2 },
+        ease: "back.out(1.7)",
+      },
+      "-=1.7"
+    );
 });
 
 ScrollTrigger.addEventListener("scrollStart", () => {
   ScrollTrigger.refresh();
 });
- */
